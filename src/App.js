42cf6d57403054bb28a96f11e5042c2e246c32ed
Viplav Jha <Viplavjha1993@gamil.com>
2021-01-11 23:12:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import firebase from "./firebase"
 
 function App() {
+  const [user ,setUser] = useState()
+  const [email ,setEmail] = useState()
+  const [password ,setPassword] = useState()
+  const [emailError ,setEmailError] = useState()
+  const [passwordError ,setPasswordError] = useState()
+  const [hasAccount ,sethasAccount] = useState(false)
+
+  const handleLogin = () =>{
+     firebase
+     .auth()
+     .signInWithEmailAndPassword(email, password)
+     .catch(err => {
+       switch(err.code) {
+         case "auth/invalide-email":
+         case "auth/user-disable":
+         case "auth/user-not-found":
+           setEmailError(err.message);
+           break;
+           case "auth/wrong-password":
+             setPasswordError(err.message);
+             break;
+ 
+       }
+
+     })
+  }
+   
+  const handleSignup =() =>{
+    firebase
+    .auth()
+    .createUserWithEmailAndPassword(email,password)
+    .catch(err => {
+      switch(err.code){
+        case "auth/email-already-in-use":
+        case "auth/invalide-email":
+          setEmailError(err.message);
+          break;
+          case "auth/Weak-Password"
+          setPasswordError(err.message);
+          break
+
+      }
+    })
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>   
     </div>
   );
 }
 
 export default App;
+
+
+
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDrZ_sgVv2r9IAqZVblVa0r_6gvLgUi89M",
+    authDomain: "login-b8e60.firebaseapp.com",
+    projectId: "login-b8e60",
+    storageBucket: "login-b8e60.appspot.com",
+    messagingSenderId: "345253387600",
+    appId: "1:345253387600:web:6345bfc8921e04018f2055",
+    measurementId: "G-HPY626NNJD"
+  };
